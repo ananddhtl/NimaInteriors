b9483bcf304d88726783bac08e8eced1fcaa8afd
unknown <anddhital@gmail.com>
2024-05-05 13:34:01 +0545

    Admin Dashboard Added

---
+++ resources/views/backend/include/header.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light" data-menu-color="brand" data-topbar-color="light">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Dashboard | Dashtrap - Responsive Bootstrap 5 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Myra Studio" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <link href="{{asset('backend/assets/libs/morris.js/morris.css')}}" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="{{asset('backend/assets/css/style.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css">
+    <script src="{{asset('backend/assets/js/config.js')}}"></script>
+</head>
---
+++ resources/views/backend/include/scripts.blade.php
@@ -0,0 +1,16 @@
+
+<script src="{{asset('backend/assets/js/vendor.min.js')}}"></script>
+<script src="{{asset('backend/assets/js/app.js')}}"></script>
+
+<!-- Knob charts js -->
+<script src="{{asset('backend/assets/libs/jquery-knob/jquery.knob.min.js')}}"></script>
+
+<!-- Sparkline Js-->
+<script src="{{asset('backend/assets/libs/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+
+<script src="{{asset('backend/assets/libs/morris.js/morris.min.js')}}"></script>
+
+<script src="{{asset('backend/assets/libs/raphael/raphael.min.js')}}"></script>
+
+<!-- Dashboard init-->
+<script src="{{asset('backend/assets/js/pages/dashboard.js')}}"></script>
